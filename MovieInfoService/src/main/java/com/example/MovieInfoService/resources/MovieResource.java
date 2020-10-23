7bf7d847a49013046948be27154ff8bcafe59ce0
rudyleiva93 <leivarudy93@gmail.com>
2020-10-23 13:58:59 -0700

    Fix PathVariable movieId typo

--- MovieInfoService/src/main/java/com/example/MovieInfoService/resources/MovieResource.java
+++ MovieInfoService/src/main/java/com/example/MovieInfoService/resources/MovieResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieResource {
 
     @RequestMapping("/{movieId}")
-    public Movie getMovieInfo(@PathVariable("movieID") String movieId) {
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
         return new Movie(movieId, "Test name");
     }
 }
